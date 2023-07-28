4184c0fc929e8da8157928347829df522a9f6cb8
lrukca <109416130+lrukca@users.noreply.github.com>
2023-07-28 10:49:54 +0900

    물건 배정 퍼블 진행중

--- frontend/src/views/admin/views/Product/selectProduct.vue
+++ frontend/src/views/admin/views/Product/selectProduct.vue
@@ -1,9 +1,56 @@
 <template>
-	<div>2222</div>
+	<div class="mt-7">
+		<v-layout align-center justify-end class="header_search">
+			<v-flex xs1 class="mr-2">
+				<selectBox :sel="productFilter1" style="font-size:13px;"></selectBox>
+			</v-flex>
+			<v-flex xs1 class="mr-2">
+				<selectBox :sel="productFilter2" style="font-size:13px"></selectBox>
+			</v-flex>
+			<v-flex xs1 class="mr-2">
+				<selectBox :sel="productFilter3" style="font-size:13px"></selectBox>
+			</v-flex>
+			<v-btn class="ml-3 search_btn" color="#009dac">적용</v-btn>
+		</v-layout>
+	</div>
 </template>
 
 <script>
-export default {}
+import { selectBox } from '@/components/index.js'
+
+export default {
+	components: {
+		selectBox,
+	},
+	data() {
+		return {
+			productFilter1: {
+				placeholder: '주택형 선택',
+				value: '',
+				items: [],
+				hideDetail: true,
+				outlined: true,
+				class: 'searchSel',
+			},
+			productFilter2: {
+				placeholder: '동 선택',
+				value: '',
+				items: [],
+				hideDetail: true,
+				outlined: true,
+				class: 'searchSel',
+			},
+			productFilter3: {
+				placeholder: '호 선택',
+				value: '',
+				items: [],
+				hideDetail: true,
+				outlined: true,
+				class: 'searchSel',
+			},
+		}
+	},
+}
 </script>
 
-<style></style>
+<style lang="scss"></style>
