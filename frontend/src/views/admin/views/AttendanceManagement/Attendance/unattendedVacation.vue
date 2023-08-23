f5602841f050ac1b1595887137e28ad55a58ead3
Taeparu <104760818+Deephthalene@users.noreply.github.com>
2023-08-24 03:27:10 +0900

    미처리 연차신청 작업중

--- frontend/src/views/admin/views/AttendanceManagement/Attendance/unattendedVacation.vue
+++ frontend/src/views/admin/views/AttendanceManagement/Attendance/unattendedVacation.vue
@@ -16,7 +16,7 @@
 						</span>
 					</v-layout>
 
-					<datatable :datatable="attendanceTable" class="table_header datatablehover3"></datatable>
+					<datatable :datatable="unattendedTable" @click="checkVacationData" class="table_header datatablehover3"></datatable>
 				</v-flex>
 				<v-flex xs4 class="pl-11">
 					<v-layout align-center>
@@ -34,20 +34,8 @@
 									</v-flex>
 								</v-layout>
 							</v-flex>
-							<v-flex class="nomal-input-type px-2" xs9 v-if="index === 2">
-								<v-layout align-center>
-									<v-flex xs4>
-										<txtField class="bizInput" v-model="right.value" :txtField="right.txtfield"></txtField>
-									</v-flex>
-									<v-flex xs1 style="text-align:center;">
-										~
-									</v-flex>
-									<v-flex xs4>
-										<txtField class="bizInput" v-model="right.value2" :txtField="right.txtfield"></txtField>
-									</v-flex>
-								</v-layout>
-							</v-flex>
-							<v-flex class="nomal-input-type px-2" xs9 v-else>
+
+							<v-flex class="nomal-input-type px-2" xs9>
 								<txtField class="bizInput" v-model="right.value" :txtField="right.txtfield"></txtField>
 							</v-flex>
 						</v-layout>
@@ -68,7 +56,7 @@
 								</v-layout>
 							</v-flex>
 							<v-flex class="nomal-input-type px-2" xs9 v-if="index === 0">
-								<v-radio-group v-model="right.radio" :disabled="setdialog.editData.vacation !== 'waiting'" row class="project_message">
+								<v-radio-group v-model="right.radio" row class="project_message">
 									<v-radio label="승인" value="agree" color="#2699FB" @click="click_radio()"></v-radio>
 									<v-radio label="반려" value="disagree" color="#2699FB" @click="click_radio()"></v-radio>
 								</v-radio-group>
@@ -80,12 +68,9 @@
 					</div>
 					<v-layout wrap class="pt-10">
 						<v-flex xs12 class="ml-auto mb-8">
-							<v-layout justify-end v-if="setdialog.editData.vacation === 'waiting'">
-								<v-btn @click="setdialog.dialog = false" dense width="100" height="26" dark color="#5B5B5B" class="mr-3">취소</v-btn>
+							<v-layout justify-end>
+								<v-btn @click="setdialog.dialog = false" dense width="100" height="26" dark color="#5b5b5b" class="mr-3">확인</v-btn>
 								<v-btn @click="businessAdd" dense width="100" height="26" dark color="#0500B7">저장</v-btn>
-							</v-layout>
-							<v-layout justify-end v-else>
-								<v-btn @click="setdialog.dialog = false" dense width="100" height="26" dark color="#0500B7" class="mr-3">확인</v-btn>
 							</v-layout>
 						</v-flex>
 					</v-layout>
@@ -128,16 +113,16 @@ export default {
 				saveBtnText: '저장',
 				modalIcon: 'success',
 			},
-			attendanceTable: {
+			unattendedTable: {
 				itemKey: 'date',
 				headers: [
-					{ text: '이름', value: 'date' },
-					{ text: '연락처', value: 'startWork' },
-					{ text: '지점', value: 'endWork' },
-					{ text: '팀', value: 'len' },
-					{ text: '근무기간', value: 'workStatus' },
-					{ text: '내용', value: 'workStatus' },
-					{ text: '비고', value: 'workStatus' },
+					{ text: '이름', value: 'username' },
+					{ text: '연락처', value: 'phoneNumber' },
+					{ text: '지점', value: 'businessName' },
+					{ text: '팀', value: 'team_rank' },
+					{ text: '근무기간', value: 'duration' },
+					{ text: '내용', value: 'vacationType' },
+					{ text: '비고', value: 'detailEtc' },
 				],
 				class: 'datatablehover3',
 				items: [],
@@ -148,106 +133,6 @@ export default {
 				pageCount: 0,
 			},
 
-			leftInfoTop: [
-				{
-					value: '',
-					title: '이름',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '',
-						readonly: true,
-					},
-					required: true,
-				},
-				{
-					value: '',
-					title: '연락처',
-					txtfield: {
-						maxlength: '15',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '예) 010-1234-5678',
-						readonly: true,
-					},
-					required: true,
-				},
-				{
-					value: '',
-					title: '영업번호',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '예) 010-1234-5678',
-						readonly: true,
-					},
-					required: false,
-				},
-				{
-					value: '',
-					title: '사업자 정보',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						readonly: true,
-					},
-					required: false,
-				},
-				{
-					value: '',
-					title: '등록일',
-					txtfield: {
-						maxlength: '15',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '예) 2023-01-01',
-						readonly: true,
-					},
-					required: false,
-				},
-				{
-					title: '팀',
-					value: '',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '',
-						readonly: true,
-					},
-
-					required: true,
-				},
-				{
-					title: '근무기간',
-					value: '',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						placeholder: '',
-						readonly: true,
-					},
-					required: true,
-				},
-			],
 			rightInfoTop: [
 				{
 					value: '',
@@ -274,19 +159,7 @@ export default {
 						readonly: true,
 					},
 				},
-				{
-					value: '',
-					value2: '',
-					title: '기간',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: true,
-						errorMessage: '',
-						autocomplete: 'off',
-						readonly: true,
-					},
-				},
+
 				{
 					value: '',
 					title: '사유',
@@ -380,6 +253,12 @@ export default {
 			}
 		},
 
+		checkVacationData(val) {
+			this.rightInfoTop[0].value = val.created_at
+			this.rightInfoTop[1].value = val.vacationType
+			this.rightInfoTop[2].value = val.vacationReason
+		},
+
 		async businessAdd() {
 			if (this.rightInfoBottom[0].radio === 'disagree' && this.rightInfoBottom[1].value === '') {
 				this.sweetInfo.title = '반려사유 에러'
@@ -396,7 +275,6 @@ export default {
 				status_check: ['startWork', 'endWork'],
 			}
 			await this.$store.dispatch('gotoWork', input).then(res => {
-				console.log(res)
 				this.leftInfoTop[6].value = res.gotoworksConnection.aggregate.count + '일'
 				this.$store.state.loading = false
 			})
@@ -431,46 +309,53 @@ export default {
 		},
 		async viewUsers(input) {
 			await this.$store.dispatch('users', input).then(res => {
-				let list = []
-				for (let i = 0; i < res.users.length; i++) {
-					this.userIDArr.push(res.users[i].id)
-				}
+				const currentDate = new Date()
+
 				res.users.forEach(element => {
-					let listData = {}
-					listData.all = element
-					listData.id = element.id
-					listData.data1 = element.username
-					listData.data2 = element.phoneNumber ? element.phoneNumber.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`) : '-'
-					listData.data3 = '-'
-					listData.data4 = '-'
-					listData.data5 = '미확인'
-					listData.gotoworksAll = []
-					listData.vacations = []
-					listData.salesPhoneNumber = element.salesPhoneNumber
-						? element.salesPhoneNumber.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)
-						: '-'
-					listData.created_at = this.$moment(element.created_at).format('YYYY-MM-DD')
-					listData.teamID = element.teamID ? element.teamID : '-'
-					listData.rankID = element.rankID ? element.rankID : '-'
-					listData.history = element.history ? element.history : []
-					listData.data5 === '미확인' ? (listData.vacation = '-') : ''
-					list.push(listData)
-					this.attendanceTable.total = res.usersConnection.aggregate.count
-					this.attendanceTable.page = input.page
+					this.vacationData.forEach((item, index) => {
+						if (item.userID === element.id) {
+							this.vacationData[index]['username'] = element.username
+							this.vacationData[index]['phoneNumber'] = element.phoneNumber
+								? element.phoneNumber.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`)
+								: '-'
+							this.vacationData[index]['businessID'] = element.businessID
+							let itemName = ''
+							for (let i = 0; i < this.$store.state.businessSelectBox.items.length; i++) {
+								if (this.$store.state.businessSelectBox.items[i].id === element.businessID) {
+									itemName = this.$store.state.businessSelectBox.items[i].name
+									break
+								} else {
+									itemName = '-'
+								}
+							}
+							const createdAtDate = new Date(element.created_at)
+							const timeDiff = Math.abs(currentDate - createdAtDate)
+							const daysPassed = Math.ceil(timeDiff / (1000 * 60 * 60 * 24))
+							const yearsPassed = Math.floor(daysPassed / 365)
+							const monthsPassed = Math.floor((daysPassed % 365) / 30)
+							const remainingDays = daysPassed % 30
+							const formattedDuration = `${yearsPassed}년 ${monthsPassed}개월 ${remainingDays}일`
+							this.vacationData[index]['duration'] = formattedDuration
+							this.vacationData[index]['businessName'] = itemName
+							this.vacationData[index]['teamID'] = element.teamID
+							this.vacationData[index]['rankID'] = element.rankID
+
+							this.unattendedTable.page = input.page
+						}
+					})
 				})
-				this.userLists = list
 			})
 		},
 		async gotoworksView(input2) {
 			await this.$store.dispatch('gotoWork', input2).then(res2 => {
 				res2.gotoworks.forEach(element2 => {
-					let workIndex = this.userLists.findIndex(item => item.id === element2.userID)
-					this.userLists[workIndex]['gotoworksAll'] = element2 ? element2 : []
-					this.userLists[workIndex]['data3'] = element2.startWork !== null ? this.$moment(element2.startWork)._i.slice(0, 5) : '-'
+					let workIndex = this.vacationData.findIndex(item => item.userID === element2.userID)
+					this.vacationData[workIndex]['gotoworksAll'] = element2 ? element2 : []
+					this.vacationData[workIndex]['data3'] = element2.startWork !== null ? this.$moment(element2.startWork)._i.slice(0, 5) : '-'
 					this.startTime = element2.startWork !== null ? this.$moment(element2.startWork)._i.slice(0, 5) : '-'
-					this.userLists[workIndex]['data4'] = element2.endWork !== null ? this.$moment(element2.endWork)._i.slice(0, 5) : '-'
+					this.vacationData[workIndex]['data4'] = element2.endWork !== null ? this.$moment(element2.endWork)._i.slice(0, 5) : '-'
 					this.endTime = element2.endWork !== null ? this.$moment(element2.endWork)._i.slice(0, 5) : '-'
-					this.userLists[workIndex]['data5'] =
+					this.vacationData[workIndex]['data5'] =
 						element2.status === 'endWork'
 							? '퇴근'
 							: element2.status === 'afternoonVacation'
@@ -481,43 +366,57 @@ export default {
 							? '휴가'
 							: '출근'
 					if (element2.status === 'vacation') {
-						this.userLists[workIndex]['data6'] = true
-						this.userLists[workIndex]['data7'] = true
-						this.userLists[workIndex]['data8'] = '-'
+						this.vacationData[workIndex]['data6'] = true
+						this.vacationData[workIndex]['data7'] = true
+						this.vacationData[workIndex]['data8'] = '-'
 					} else {
-						this.userLists[workIndex]['data6'] = element2.startWork ? true : false
-						this.userLists[workIndex]['data7'] = element2.endWork ? true : false
+						this.vacationData[workIndex]['data6'] = element2.startWork ? true : false
+						this.vacationData[workIndex]['data7'] = element2.endWork ? true : false
 					}
 
 					// if (element2.startWork && element2.endWork) {
 					// 	this.userLists[workIndex]['data8'] = this.timeCheck(element2.startWork, element2.endWork)
 					// }
 				})
+				this.unattendedTable.items = this.vacationData
 			})
 		},
 		async vacationView(item) {
 			await this.$store.dispatch('vacations', item).then(res => {
-				console.log(res)
+				this.unattendedTable.total = res.vacations.length
+				let list = []
+				for (let i = 0; i < res.vacations.length; i++) {
+					this.userIDArr.push(res.vacations[i].userID)
+				}
 				res.vacations.forEach(el => {
-					this.vacationData = el
-					let workIndex = this.userLists.findIndex(item => item.id === el.userID)
-					this.userLists[workIndex]['vacations'] = el
-					// this.userLists[workIndex]['vacationStart'] = el.start
-					// this.userLists[workIndex]['vacationEnd'] = el.end
-					this.userLists[workIndex]['vacationDate'] = el.date
-					this.userLists[workIndex]['vacationReason'] = el.vacationReason
-					this.userLists[workIndex]['vacation'] = el.vacationStatus
-					this.userLists[workIndex]['vacationType'] = el.vacationType
-					this.userLists[workIndex]['vacationCreated_at'] = el.created_at
-					this.userLists[workIndex]['vacationID'] = el.id
-					this.userLists[workIndex]['vacationRejectComment'] = el.rejectComment
+					let listData = {}
+					listData.all = el
+					listData.id = el.id
+					listData.date = el.date
+					listData.created_at = this.$moment(el.created_at).format('YYYY-MM-DD')
+					listData.userID = el.userID
+					listData.vacationReason = el.vacationReason
+					listData.vacationStatus = el.vacationStatus
+					listData.vacationType =
+						el.vacationType === 'vacation'
+							? '연차'
+							: el.vacationType === 'morningVacation'
+							? '오전반차'
+							: el.vacationType === 'afternoonVacation'
+							? '오후반차'
+							: el.vacationType === 'sick'
+							? '병가'
+							: ''
+					listData.rejectComment = el.rejectComment
+					listData.gotoworksAll = []
+					list.push(listData)
 				})
-				console.log(this.vacationData)
+				this.vacationData = list
 			})
 		},
 		async dataSetting() {
-			for (let index = 0; index < this.userLists.length; index++) {
-				const element = this.userLists[index]
+			for (let index = 0; index < this.vacationData.length; index++) {
+				const element = this.vacationData[index]
 				let teamData = this.teamData.filter(x => x.id === element.teamID)[0]
 				let rankData = this.rankData.filter(x => x.id === element.rankID)[0]
 
@@ -536,53 +435,40 @@ export default {
 				} else {
 					element.team_rank = '-'
 				}
-				element.salesPhoneNumber_txtField = {
-					value: '',
-					txtfield: {
-						maxlength: '255',
-						outlined: true,
-						hideDetail: false,
-						errorMessage: '',
-						placeholder: '',
-					},
-				}
-				element.workingStatusName = element.workingStatus ? '재직' : '퇴사'
-				element.created_at_format = this.$moment(element.created_at).format('YYYY년MM월DD일')
+
 				element.teamItems = this.teamData
 				element.rankItems = this.rankData
 			}
-			this.attendanceTable.items = JSON.parse(JSON.stringify(this.userLists))
-			this.attendanceTable.origin_items = JSON.parse(JSON.stringify(this.userLists))
+			this.unattendedTable.items = JSON.parse(JSON.stringify(this.vacationData))
 		},
 	},
 	async created() {
 		await this.me()
 		await this.getTeams()
 		await this.getRanks()
+		let input3 = {
+			start: 0,
+			limit: 10,
+			vacationStatus: 'waiting',
+		}
 		let input = {
 			start: 0,
 			limit: 10,
 			roleName: 'Counselor',
 			businessID: this.$store.state.businessSelectBox.value,
+			idArr: this.userIDArr,
 		}
+		await this.vacationView(input3)
 		await this.viewUsers(input)
 		let input2 = {
 			start: 0,
 			limit: 10,
-			date: this.$moment().format('YYYY-MM-DD'),
 			roleName: 'Counselor',
 			userID: this.userIDArr,
 		}
-		let input3 = {
-			start: 0,
-			limit: 10,
-			vacationStatus: 'waiting',
-			userID: this.userIDArr,
-		}
+
 		await this.gotoworksView(input2)
-		await this.vacationView(input3)
-		console.log(this.userLists)
-		// await this.dataSetting()
+		await this.dataSetting()
 	},
 }
 </script>
